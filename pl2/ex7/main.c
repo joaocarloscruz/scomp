756e9eaad070befc11eefafa7141d77129fabb8e
joaocarloscruz <joaocruzbusiness@gmail.com>
2025-04-25 17:45:42 +0100

    add pl2 ex7

--- pl2/ex7/main.c
+++ pl2/ex7/main.c
@@ -6,109 +6,104 @@
 #include <string.h>
 #include <signal.h>
 #include <errno.h>
+#include <time.h>
 
-#define MAX 50
+#define NUM_PROCESSES 50
 
-volatile int finished_count = 0;
 volatile int success_count = 0;
-pid_t child_pids[MAX];
+volatile int processes_finished = 0;
 
-void parent_sig_handler(int signum) {
-    if (signum == SIGUSR1) {
-        success_count++;
-        finished_count++;
-        printf("Parent: Success %d, Finished %d\n", success_count, finished_count);
-    } else if (signum == SIGUSR2) {
-        finished_count++;
-        printf("Parent: Failure %d, Finished %d\n", success_count, finished_count);
-    }
-    if (finished_count == 25) {
-        if (success_count == 0) {
-            printf("\nParent: Inefficient Algorithm!\n");
-            for (int j = 0; j < MAX; j++) {
-                kill(child_pids[j], SIGTERM);
-            }
-        } else {
-            printf("\nParent: At least one success. Signaling children to start simulate2.\n");
-            for (int j = 0; j < MAX; j++) {
-                kill(child_pids[j], SIGUSR1);
-            }
-        }
-    }
+void handle_success(int signo) {
+    success_count++;
+    processes_finished++;
+    
 }
 
-void child_sig_handler(int signum) {
-    if (signum == SIGUSR1) {
-        printf("Child %d: Start simulate2.\n", getpid());
-    }
+void handle_failure(int signo) {
+    processes_finished++;
+   
 }
 
 int simulate1() {
-    srand(time(NULL) ^ (getpid() << 16));
-    sleep(rand() % 3);
-    int result = rand() % 2;
-    printf("Child %d: simulate1() %d\n", getpid(), result);
-    return result;
+    srand(time(NULL) ^ getpid());
+    printf("[Child %d] sim1 sleeping...\n", getpid());
+    usleep((rand() % 100000) + 100000);
+    if (((rand() % 100) + 1) == 1) {
+        return 1;
+    }
+    return 0;
 }
 
 int simulate2() {
-    srand(time(NULL) ^ (getpid() << 16));
-    sleep(rand() % 2);
-    printf("Child %d: simulate2() finished.\n", getpid());
+    printf("[Child %d] sim2 sleeping...\n", getpid());
+    usleep((rand() % 100000) + 100000);
+    if (((rand() % 100) + 1) == 1) {
+        return 1;
+    }
     return 0;
 }
 
 int main() {
+    pid_t children_pid[NUM_PROCESSES];
     pid_t pid;
-    struct sigaction act;
 
-    act.sa_handler = parent_sig_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
+    struct sigaction act1, act2;
 
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
+    memset(&act1, 0, sizeof(struct sigaction));
+    memset(&act2, 0, sizeof(struct sigaction));
+    act1.sa_handler = handle_success;
+    act2.sa_handler = handle_failure;
+    act1.sa_flags = SA_RESTART;
+    act2.sa_flags = SA_RESTART;
+    sigaction(SIGUSR1, &act1, NULL);
+    sigaction(SIGUSR2, &act2, NULL);
 
-    act.sa_handler = child_sig_handler; // Change the handler for the child
-    sigemptyset(&act.sa_mask);         // Make sure to reset the mask and flags if needed
-    act.sa_flags = 0;
-
-    int i = 0;
-
-    do {
+    for (int i = 0; i < NUM_PROCESSES; i++) {
         pid = fork();
         if (pid < 0) {
-            perror("Error in fork");
+            perror("fork failed");
             exit(EXIT_FAILURE);
         }
-        child_pids[i] = pid;
-        i++;
-    } while (i < MAX && pid > 0);
-
-    if (pid == 0) {
-        sigaction(SIGUSR1, &act, NULL); // Use the same 'act' struct in the child
-        int result_simulate1 = simulate1();
-        if (result_simulate1 == 1) {
-            kill(getppid(), SIGUSR1);
-        } else {
-            kill(getppid(), SIGUSR2);
-        }
-        printf("Child %d: simulate1() done, waiting...\n", getpid());
-        pause();
-
-        simulate2();
-        printf("Child %d: simulate2() done.\n", getpid());
-        exit(EXIT_SUCCESS);
-    }
-
-    if (pid > 0) {
-        for (int j = 0; j < MAX; j++) {
-            if (wait(NULL) == -1 && errno != ECHILD) {
-                perror("wait");
+        if (pid == 0) {
+            int res = simulate1();
+            if (res == 1) {
+                kill(getppid(), SIGUSR1);
+            } else {
+                kill(getppid(), SIGUSR2);
             }
+            raise(SIGSTOP);
+            //pause();
+            res = simulate2();
+            exit(EXIT_SUCCESS);
+        } else {
+            children_pid[i] = pid;
         }
-        printf("Parent finished.\n");
     }
 
+    while(processes_finished < 25){
+        pause();
+    }
+   
+    int flag = 0;
+    if (success_count == 0) {
+        printf("Inefficient algorithm\n");
+        flag = 1;
+    } else {
+        printf("Efficient algorithm! Heading for second simulation\n");
+    }
+
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        if (flag == 1) {
+            kill(children_pid[i], SIGKILL);
+        } else {
+            kill(children_pid[i], SIGCONT);
+        }
+    }
+
+    sleep(5);
+    printf("Finished the algorithm computation");
+    for(int i = 0; i < NUM_PROCESSES; i++){
+        waitpid(children_pid[i], NULL, 0);
+    }
     return 0;
 }

d66db5882d5894b432ece9ade7eaa69e3504adbe
joaocarloscruz18 <joaocarloscruz18@gmail.com>
2025-04-26 14:38:52 +0000

    add pl3 ex3

--- pl3/ex3/main.c
+++ pl3/ex3/main.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <>
+#include <string.h>
+#include <signal.h>
 
 #define P_READ 0
 #define P_WRITE 1
+#define SIZE 512
 
 int main(){
 	int fd[2];
 	pid_t pid;
 	char buffer[SIZE];
 	ssize_t bytesRead;
-	int filefd;
 
 	if(pipe(fd) == -1){
 		perror("pipe failed");
 		exit(EXIT_FAILURE);
-
 	}
 
 	pid = fork();
@@ -31,18 +31,37 @@ int main(){
 	//child code
 	if(pid == 0){
 		close(fd[P_WRITE]);
-		
-		
-
+		if(read(fd[P_READ], buffer, SIZE) < 0){
+			perror("error receiving data");
+			exit(EXIT_FAILURE);
+		}
+		printf("%s\n", buffer);
+		close(fd[P_READ]);
 		exit(1);
 	}
 
 	//parent code
-	
 	close(fd[P_READ]);
+	FILE *fprt;
+	fprt = fopen("text.txt", "r");
+	if(fprt == NULL){
+		perror("fopen failed");
+		kill(pid, SIGKILL);
+		exit(EXIT_FAILURE);
+	}
 
-	if(write
 
+	fgets(buffer, SIZE, fprt);
+	bytesRead = strlen(buffer);
+
+	if(write(fd[P_WRITE], buffer, bytesRead) < 0){
+		perror("error sending data");
+		kill(pid, SIGKILL);
+		exit(EXIT_FAILURE);
+	}
+	close(fd[P_WRITE]);
+	wait(NULL);
+	fclose(fprt);
 
 			
 	return 0;

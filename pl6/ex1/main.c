a32f89dc7e7d16e17bd8024e8db749ce5ab4ac5f
joaocarloscruz <joaocruzbusiness@gmail.com>
2025-05-25 10:25:06 +0100

    remove outdated simulator from sprint 2

--- pl6/ex1/main.c
+++ pl6/ex1/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <semaphore.h>
 
-void thread_func(void* arg) {
+void* thread_func(void* arg) {
     char* str = (char*)arg;
     printf("%s\n", str);
     pthread_exit(NULL);
@@ -16,12 +16,9 @@ int main() {
     pthread_t thread[2];
 
     pthread_create(&thread[0], NULL, thread_func, (void*)first_name);
-
+    pthread_join(thread[0], NULL);
     pthread_create(&thread[1], NULL, thread_func, (void*)last_name);
-
-    for (int i = 0; i < 2; i++) {
-        pthread_join(thread[i], NULL);
-    }
+    pthread_join(thread[1], NULL);
 
     return 0;
 }
